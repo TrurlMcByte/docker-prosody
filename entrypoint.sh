37ed9c9f7aadfa90805d87faa01de7014f1d06e7
Trurl McByte <trurl@mcbyte.net>
2016-05-29 21:08:31 +0300

    generated config + admin

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,7 +8,7 @@ set -e
 
 if [ "$LOCAL" -a  "$PASSWORD" -a "$DOMAIN" ] ; then
     sed -i "s/example.com/${DOMAIN}/" /etc/prosody/prosody.cfg.lua
-    sed -i "s/admins = \{ \}/admins = \{ ${LOCAL}@${DOMAIN} \}/" /etc/prosody/prosody.cfg.lua
+    sed -i "s/admins = { }/admins = { ${LOCAL}@${DOMAIN} }/" /etc/prosody/prosody.cfg.lua
     prosodyctl register $LOCAL $DOMAIN $PASSWORD
 fi
 

bf04fbaad1c0ff9b2b1358d3ffbf619dde5825c2
Trurl McByte <trurl@mcbyte.net>
2016-07-13 18:11:29 +0300

    fix for separate volumes

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,8 +9,12 @@ set -e
 if [ "$LOCAL" -a  "$PASSWORD" -a "$DOMAIN" -a ! -f /etc/prosody/.local ] ; then
     sed -i "s/example.com/${DOMAIN}/" /etc/prosody/prosody.cfg.lua
     sed -i "s/specialadminuser@/${LOCAL}@/" /etc/prosody/prosody.cfg.lua
-    prosodyctl register $LOCAL $DOMAIN $PASSWORD
+
+    if test -f /var/lib/prosody/.local; then
+        prosodyctl register $LOCAL $DOMAIN $PASSWORD
+    fi
     touch /etc/prosody/.local
+    touch /var/lib/prosody/.local
 fi
 
 exec "$@"
